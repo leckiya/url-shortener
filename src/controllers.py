540879ceb3b99d956df755a19d37241968613c56
al3xhan <gh.han.2023@mitb.smu.edu.sg>
2025-02-09 15:12:10 +0800

    feat: add endpoint to redirect user based on the key (#9)

--- src/controllers.py
+++ src/controllers.py
@@ -1,8 +1,9 @@
 from typing import Annotated
 from fastapi import APIRouter, Body, HTTPException, Request
+from fastapi.responses import RedirectResponse
 from pydantic import BaseModel, Field
 from sqlalchemy import delete, select, update
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, NoResultFound
 from models import Url
 from database import request_session
 
@@ -10,9 +11,13 @@ from database import request_session
 router = APIRouter()
 
 
+KeyField = Field(min_length=1, max_length=64)
+TargetField = Field(min_length=1, max_length=256)
+
+
 class UrlObject(BaseModel):
-    key: str = Field(min_length=1, max_length=64)
-    target: str = Field(min_length=1, max_length=256)
+    key: str = KeyField
+    target: str = TargetField
 
 
 def url_object_from_database(url: Url) -> UrlObject:
@@ -68,7 +73,7 @@ async def create_url(
         404: {"description": "Key does not exists"},
     },
 )
-async def delete_url(request: Request, key: str) -> UrlObject:
+async def delete_url(request: Request, key: Annotated[str, KeyField]) -> UrlObject:
     async with request_session(request) as session:
         async with session.begin():
             stmt = delete(Url).where(Url.key == key).returning(Url)
@@ -94,8 +99,10 @@ class UrlUpdateChangeset(BaseModel):
         404: {"description": "Key does not exists"},
     },
 )
-async def update_rul(
-    request: Request, key: str, param: Annotated[UrlUpdateChangeset, Body()]
+async def update_url(
+    request: Request,
+    key: Annotated[str, KeyField],
+    param: Annotated[UrlUpdateChangeset, Body()],
 ) -> UrlObject:
     async with request_session(request) as session:
         async with session.begin():
@@ -112,3 +119,20 @@ async def update_rul(
                 raise HTTPException(status_code=404)
 
             return url_object_from_database(updated)
+
+
+@router.get(
+    "/redirect/{key}",
+    response_model=None,
+    status_code=308,
+    responses={404: {"description": "Key does not exists"}},
+)
+async def redirect(request: Request, key: Annotated[str, KeyField]) -> RedirectResponse:
+    async with request_session(request) as session:
+        async with session.begin():
+            try:
+                url = await session.get_one(Url, key)
+                print(url.target)
+                return RedirectResponse(url=url.target, status_code=308)
+            except NoResultFound:
+                raise HTTPException(status_code=404)
